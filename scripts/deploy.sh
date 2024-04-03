8781f984237c79554322463e56a9b8ef5ee4c204
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-04-03 13:55:37 +0100

    add debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,21 @@ BUILD_DIR=${PROJECT_DIR}/app/build
 
 . ${BUILD_DIR}/buildinfo
 
+
 cd ${PROJECT_DIR}
+
+echo "current working directory"
+pwd
+ls -al
+
+echo "PROJECT_DIR: ${PROJECT_DIR}"
+ls -al ${PROJECT_DIR}
+
+echo "BUILD_DIR: ${BUILD_DIR}"
+ls -al ${BUILD_DIR}
+
+echo "id: $(id -a)"
+echo "HOME: ${HOME}"
+ls -al ${HOME}
 
 ./gradlew publish -DrepositoryName=${REPOSITORY} -DprojectVersion=${VERSION} 
