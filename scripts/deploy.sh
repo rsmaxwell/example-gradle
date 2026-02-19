04e60f4dc1428923dc9a8b135cd5583c5b034892
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-02-19 17:28:21 +0000

    Improve auto versioning

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,14 @@ BUILD_DIR=${PROJECT_DIR}/app/build
 
 cd ${PROJECT_DIR}
 
+
+set -x
+whoami
+echo "home: $HOME"
+ls -al /home/gradle/.gradle
+cat /home/gradle/.gradle/gradle.properties
+set +x
+
 ./gradlew publish --no-daemon --info \
     -PrepositoryName="${REPOSITORY}" \
     -PprojectVersion="${VERSION}" \
